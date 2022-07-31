ad313c01274b8236f4571ef79e9244d965e4d9b2
yoratoni <dev.bellerstudio@gmail.com>
2022-07-31 11:24:47 +0200

    General React setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles/fonts.css";
+import "./styles/main.css";
+
+
+const App = () => {
+    return (
+        <>
+        </>
+    );
+};
+
+
+export default App;
