e6c56c986e71d3402603ac17e1daef0ab3249a38
yoratoni <71235233+yoratoni@users.noreply.github.com>
2022-08-08 19:50:17 +0200

    Clean setup

--- src/components/layouts/Content.tsx
+++ src/components/layouts/Content.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import "./Content.css";
 
-import CollatzCanvas from "../CollatzCanvas";
 
 const Content = () => {
     return (
         <div className="content">
-            <div id="collatz-canvas-container">
-                <CollatzCanvas />
-            </div>
+
         </div>
     );
 };
--- src/typescript/interfaces.d.ts
+++ src/typescript/interfaces.d.ts
@@ -1,43 +1,5 @@
 declare global {
-    /**
-     * Contains the coordinates of each line
-     * and the number inside the sequence,
-     * used for the alpha calculation
-     */
-    interface ICollatzLineData {
-        x1: number,
-        y1: number,
-        x2: number,
-        y2: number,
-        number: number,
-        hue: number
-    }
 
-
-    /**
-     * The main cache for the Collatz visualization
-     */
-    interface ICollatzCache {
-        longestSequence: number,
-        lastWorkerSequence: number,
-        tempRecordSequence: number,
-        sequenceSignatures: number[],
-        history: number[ICollatzLineData],
-        lastWindowDims: number[]
-    }
-
-
-    /**
-     * Each worker draw one sequence (1 line per frame)
-     */
-    interface ICollatzWorker {
-        sequence: number,
-        angle: number,
-        array: number[],
-        index: number,
-        lineLength: number,
-        lineData: ICollatzLineData
-    }
 }
 
 
