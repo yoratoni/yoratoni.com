92590ce42602343af4b50a984dab401b9599f6d3
yoratoni <71235233+yoratoni@users.noreply.github.com>
2022-09-18 18:16:21 +0200

    General Update

--- src/components/organisms/header/Header.tsx
+++ src/components/organisms/header/Header.tsx
@@ -5,14 +5,7 @@ import "./Header.css";
 const Header = () => {
     return (
         <header className="header">
-            <div className="header__section-container">
-                <div className="header__vertical-rule"></div>
-
-                <div className="header__title-container">
-                    <h1>ADRIEN</h1>
-                    <h1>BIBOLLET</h1>
-                </div>
-            </div>
+            <h1>ADRIEN BIBOLLET</h1>
         </header>
     );
 };
