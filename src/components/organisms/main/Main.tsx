5eecf3bede38f14e1285104d6f9499dbdf9ec1ea
yoratoni <71235233+yoratoni@users.noreply.github.com>
2022-09-28 23:52:14 +0200

    About Me

--- src/components/organisms/main/Main.tsx
+++ src/components/organisms/main/Main.tsx
@@ -11,7 +11,48 @@ const Main = () => {
                 heading="About Me"
                 subheading="Me, myself and I"
             >
-                Pute
+                <div className="main__about-me">
+                    <p>
+                        Hi my name is Adrien, I&apos;m a freelance and a full-stack web
+                        developer based in France who loves to help people and communities by creating
+                        unique websites and a lot of other stuff but let me give you too much
+                        information about how my freelance career started:
+                    </p>
+
+                    <p>
+                        My interest in art started back when I discovered a conference on Youtube called
+                        <a
+                            href="https://www.youtube.com/watch?v=6avJHaC3C2U"
+                            hrefLang="en"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            &nbsp;The&nbsp;Art&nbsp;Of&nbsp;Code
+                        </a>,
+                        I immediately loved the idea of creating a whole Universe
+                        of visualizations with code. It also gave me a lot of
+                        practice with different softwares and techs
+                        (Yeah, I did that almost 8 hours/day during weekends).
+                    </p>
+                    <p>
+                        Fast-forward to today, I&apos;m specializing in building
+                        art-focused incredible digital experiences for a variety of clients
+                        including myself on the weekends.
+                    </p>
+
+                    <div className="main__skills">
+                        <p>Here&apos;s some technologies that I use pretty frequently:</p>
+
+                        <ul>
+                            <li>Javascript (ES6+)</li>
+                            <li>TypeScript</li>
+                            <li>Node JS</li>
+                            <li>Python</li>
+                            <li>Apache</li>
+                            <li>React</li>
+                        </ul>
+                    </div>
+                </div>
             </MainSection>
 
             <MainSection
