eb5129d0e9f296067f01afae4b8952e3223e5a5d
yoratoni <71235233+yoratoni@users.noreply.github.com>
2022-11-10 17:18:34 +0100

    General update

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -9,7 +9,7 @@ const Home = () => {
             <main className="pages__main">
                 <header className="pages__header">
                     <h1 className="pages__header-title">
-                    adrien bibollet
+                    ADRIEN BIBOLLET
                     </h1>
                     <h3 className="pages__header-subtitle">
                         &#91;full stack web developer&#93;
