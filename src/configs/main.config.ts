8332313e487a9496098778f6051da19555d367e3
yoratoni <71235233+yoratoni@users.noreply.github.com>
2023-10-05 09:42:06 +0200

    WIP

--- src/configs/main.config.ts
+++ src/configs/main.config.ts
@@ -23,6 +23,26 @@ const config = {
         contentHeightWhereSpeedFactorIsOne: 700,
         minContentHeightSpeedFactor: 0.2
     },
+
+    // About
+    about: [
+        // P1
+        `I started programming in <b>2015</b>, after doing a lot of <b>DIY</b> projects in my workshop,
+        I wanted to create stuff without being limited by the physical world (aka. money).
+
+        I discovered that it was possible to create anything I wanted with a computer,
+        no need for money or tools anymore, a lot of people were dedicating their time to
+        build open-source tools and libraries that anyone could use for free, and I wanted
+        to be part of this community.
+
+        I started to learn <b>GML</b>, a programming language for a game engine called <b>Game Maker 8</b>.`,
+
+        // P2
+        `A`,
+
+        // P3
+        `B`
+    ]
 };
 
 
--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,30 +1,24 @@
+import { Interweave } from "interweave";
+
 import Section from "@/components/base/Section";
 import Title from "@/components/base/Title";
+import config from "@/configs/main.config";
 
 
 export default function About({ pageIndex }: { pageIndex: number; }) {
     return (
         <Section>
-            <Title
-                top="Curious?"
-                title="MORE ABOUT ME"
-                bottom="How it started.."
-            />
+            {pageIndex === 0 && (
+                <Title
+                    top="Curious?"
+                    title="MORE ABOUT ME"
+                    bottom="How it started.."
+                />
+            )}
 
             <div className="flex items-center justify-center w-full pb-6">
                 <p className="w-full h-auto max-w-2xl px-6 text-[15px] whitespace-pre-wrap md:text-xl">
-                    I started programming in 2015, after doing a lot of DIY projects in my workshop,
-                    I wanted to create stuff without being limited by the physical world (aka. money).
-                    <br /><br />
-
-                    I discovered that it was possible to create anything I wanted with a computer,
-                    no need for money or tools anymore, a lot of people were dedicating their time to
-                    build open-source tools and libraries that anyone could use for free, and I wanted
-                    to be part of this community.
-
-                    A few years later, after a lot of self-learning and beginner projects,
-                    I decided to learn web development.
-                    <br /><br />
+                    <Interweave content={config.about[pageIndex]} />
                 </p>
             </div>
         </Section>
